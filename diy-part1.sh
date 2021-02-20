7137c1b4f861edf569e9dcf158f413021db58412
ZhouYusen <58332209+zys91@users.noreply.github.com>
2021-02-20 10:06:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,5 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# Add Lienol source (PassWall) (need remove clash packages)
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
