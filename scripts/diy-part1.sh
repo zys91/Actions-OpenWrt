7a86ebef1f5adc92d4b0b7bba32dba14cf12c646
919129850 <919129850@163.com>
2021-08-03 22:25:19 +0800

    Update config v20210803-1

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -10,8 +10,8 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Add Helloworld source
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add Lienol source (need remove clash packages)
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
